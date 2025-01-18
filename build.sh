cad42c4a63c11577c0b53cb6b39a042d40413312
zengjianping <happysjfzjp@126.com>
2025-01-18 14:04:16 +0800

    dev

--- build.sh
+++ build.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
+pip install "pybind11[global]"
 rm -r build
 mkdir build && cd build
-#nvidia-smi --query-gpu=compute_cap --format=csv
-cmake -DTENSORRT_PATH=/home/zengjianping/Programs/TensorRT-10.7.0.23 -DCMAKE_CUDA_ARCHITECTURES=89 ..
+cuda_arch=$(nvidia-smi --query-gpu=compute_cap --format=csv | tail -1 | awk '{print $1}' | sed "s/\.//g")
+cmake -DTENSORRT_PATH=/usr/local/tensorrt -DCMAKE_CUDA_ARCHITECTURES=$cuda_arch -DCMAKE_CUDA_FLAGS=-arch=sm_$cuda_arch ..
 cmake --build . -j8 --config Release
 
 
--- examples/detect/build.sh
+++ examples/detect/build.sh
@@ -3,7 +3,7 @@
 rm -r build
 mkdir build && cd build
 
-cmake -DTENSORRT_PATH="/home/zengjianping/Programs/TensorRT-10.7.0.23" -DDEPLOY_PATH="/data/ProjectZKZS/Projects/TensorRT-YOLO" ..
+cmake -DTENSORRT_PATH="/usr/local/tensorrt" -DDEPLOY_PATH="/data/ProjectZKZS/Projects/TensorRT-YOLO" ..
 cmake --build . -j8 --config Release
 
 
