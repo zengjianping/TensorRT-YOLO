116731d0b68cc48bd39c2b5b4d9a0c7229a634a8
zengjianping <happysjfzjp@126.com>
2025-01-22 16:46:07 +0800

    dev

--- scripts/onnx_to_engine.sh
+++ scripts/onnx_to_engine.sh
@@ -2,7 +2,10 @@
 
 MODEL_DIR=datas/models
 MODEL_NAME=yolo11n
+MODEL_SIZE=640x640
+MODEL_BATCH=8
 
 trtexec --onnx=$MODEL_DIR/$MODEL_NAME.onnx --saveEngine=$MODEL_DIR/$MODEL_NAME.engine --fp16 \
-    --minShapes=images:1x3x640x640 --optShapes=images:4x3x640x640 --maxShapes=images:8x3x640x640
+    --minShapes=images:1x3x${MODEL_SIZE} --optShapes=images:${MODEL_BATCH}x3x${MODEL_SIZE} \
+    --maxShapes=images:${MODEL_BATCH}x3x${MODEL_SIZE}
 
